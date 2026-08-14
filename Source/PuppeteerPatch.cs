ed3d4ddb0f23ccef0873dccbca0c92bf7c81e23d
F1reTom <>
2026-08-15 01:57:56 +0300

    init csproj

---
+++ Source/PuppeteerPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Multiplayer.API;
+using Verse;
+using System;
+
+namespace AstroMultiplayerCompability
+{
+    // Инициализация при старте игры
+    [StaticConstructorOnStartup]
+    public static class MpInit
+    {
+        static MpInit()
+        {
+            // Проверяем, запущен ли мультиплеер
+            if (!MP.enabled) return;
+
+            // Регистрируем гармонию для нашего патча
+            var harmony = new Harmony("com.astro.mpcompat.puppeteer");
+            
+            Log.Message("[Astro MpCompat] Мод-патч успешно загружен и готов к работе!");
+        }
+    }
+}
