d9653f5c6555e1e247c1a90a3271101015568f18
F1reTom <>
2026-08-17 20:47:36 +0300

    Sunset update

--- Source/SunsetTechnology3.cs
+++ Source/SunsetTechnology3.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using HarmonyLib;
 using Multiplayer.API;
 using Multiplayer.Compat; // Подключаем mpcompat
@@ -5,18 +6,24 @@ using Verse;
 
 namespace AstroMultiplayerCompability
 {
-    [MpCompatFor("sunsetmoderteam.sunsettechnology3")]
+    [MpCompatFor("sunsetmoderteam.sunsettechnology3")] // ID мода из About.xml
     public class SunsetPatch
     {
         public SunsetPatch(ModContentPack mod)
         {
             MpCompat.RegisterLambdaMethod("SUNSET3.Building_TurretGun_cy", "GetGizmos", 0);
-            MpCompat.RegisterLambdaMethod("SUNSET3.Building_Excavator_cy", "GetGizmos", 0);
 
-            AstroMpCompatUtils.IsolateNRG("SUNSET3.Building_TurretGunHasSpeed_cy:Tick_Patch1");
             AstroMpCompatUtils.IsolateNRG("SUNSET3.Building_TurretGun_cy:Tick");
-            AstroMpCompatUtils.IsolateNRG("SUNSET3.TailBullet_cy_cy:MakeTail");
+            AstroMpCompatUtils.IsolateNRG("SUNSET3.Building_TurretGunHasSpeed_cy:Tick");
+            AstroMpCompatUtils.IsolateNRG("SUNSET3.TailBullet_cy:MakeTail");
+            AstroMpCompatUtils.IsolateNRG("SUNSET3.ExpansionModule_Turret_cy:TryAttack");
 
+            AstroMpCompatUtils.IsolateNRG("SUNSET3.HediffComp_ConcentrationExplosion_cy:CompPostTick");
+            AstroMpCompatUtils.IsolateNRG("SUNSET3.HediffComp_ExecuteOnFailure_cy:CompPostTick");
+            AstroMpCompatUtils.IsolateNRG("SUNSET3.HediffComp_WeaponFlash_cy:CompPostTick");
+
+            AstroMpCompatUtils.IsolateNRG("SUNSET3.HediffComp_ConcentrationExplosion_cy:CompPostPostRemoved");
+            AstroMpCompatUtils.IsolateNRG("SUNSET3.HediffComp_ExecuteOnFailure_cy:CompPostPostAdd");
         }
     }
 }
